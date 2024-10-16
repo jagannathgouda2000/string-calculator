ad159d564668ca4e7137ed60e369bc92cc2d2edb
jagannathgouda2000 <jagannnathgouda@gmail.com>
2024-10-16 16:27:43 +0530

    task 1 completed

--- task.js
+++ task.js
@@ -1,7 +1,15 @@
-console.log("hello");
-
-function calculateSum() {
-    console.log("function initiated")
+function add() {
+    const inputString = document.querySelector("#inputBox").value.trim();
+    if(inputString.length == 0){
+        document.querySelector("#result").innerHTML = "0";
+        return  
+    }
+    const numbers = inputString.split(",");
+    const answer = numbers.reduce((result,currentvalue) => {
+        return result + parseInt(currentvalue);
+    },0)
+    document.querySelector("#result").innerHTML = answer;
+    return
 }
 
-document.getElementById("submitButton").addEventListener("click", calculateSum)
+document.getElementById("submitButton").addEventListener("click", add)
